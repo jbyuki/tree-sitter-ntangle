ddd46249218042a11f1d6d992926ff5442fd2a0a
jbyuki <jyburkhard@hotmail.com>
2021-02-16 19:35:20 +0100

    first commit

---
+++ grammar.js
@@ -0,0 +1,17 @@
+module.exports = grammar({
+	name: 'ntangle',
+	rules: {
+		source_file: $ => seq(
+			optional($.assembly_header), 
+			$.section,
+			repeat(choice(
+				$.codeline,
+				$.section,
+				$.section_ref
+			))),
+		assembly_header: $ => /##[a-zA-Z0-9_./]*?\s*?\n/,
+		codeline: $ => /\s*[^@\s].*?\s*?\n/,
+		section: $ => /@[a-zA-Z0-9_./]*?[+-]?=\s*?\n/,
+		section_ref: $ => /\s*@[a-zA-Z0-9_]*?\s*?\n/,
+	}
+});
