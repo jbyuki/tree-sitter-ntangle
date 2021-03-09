e1659fdf7d3606bb457527d61040bb241b9cf48e
jbyuki <jyburkhard@hotmail.com>
2021-03-09 21:06:12 +0100

    add minus

--- grammar.js
+++ grammar.js
@@ -9,9 +9,9 @@ module.exports = grammar({
 				$.section,
 				$.section_ref
 			))),
-		assembly_header: $ => /##[a-zA-Z0-9_./]*?\s*?\n/,
+		assembly_header: $ => /##[a-zA-Z0-9_./-]*?\s*?\n/,
 		codeline: $ => /\s*[^@\s].*?\s*?\n/,
-		section: $ => /@[a-zA-Z0-9_./*]*?[+-]?=\s*?\n/,
-		section_ref: $ => /\s*@[a-zA-Z0-9_]*?\s*?\n/,
+		section: $ => /@[a-zA-Z0-9_./*\-]*?[+-]?=\s*?\n/,
+		section_ref: $ => /\s*@[a-zA-Z0-9_-]*?\s*?\n/,
 	}
 });
